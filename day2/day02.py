101e8197134c8e19988c4f02f0fc7a9a1fcefc3b
abdullah6516 <61629432+abdullah6516@users.noreply.github.com>
2022-01-08 19:36:42 +0200

    Add files via upload

---
+++ day2/day02.py
@@ -0,0 +1,61 @@
+integer = 5 
+
+float1 = 5.5
+
+bolean = True
+
+string = 'day02' # or "day02"
+
+
+# Atithmetic operation
+
+
+Var1 = 5 ; Var2 = 3
+
+print(Var1 + Var2)
+print(Var1 - Var2)
+print(Var1 * Var2)  # multiplication
+print(Var1 ** Var2) # exponent 5 ^ 3 = 125
+print(Var1 / Var2)
+print(Var1 // Var2) # floor divition 5 // 3 = 1
+print(Var1 % Var2)  # modulus 5 % 3 = 2
+
+
+# Comparison operators
+
+print(Var1 > Var2)
+print(Var1 >= Var2)
+print(Var1 <= Var2)
+print(Var1 == Var2) #equal
+print(Var1 != Var2) #not equal
+print(Var1 is integer)
+print(Var1 is not integer)
+
+
+# boolean operators
+
+bool1 = True ; bool2 = 0 
+
+print(bool1 or bool2)
+print(bool1 and bool2)
+print(not bool2)
+
+# name = ' alan\'s book '
+
+
+# Mathmatical functions
+
+print(abs(-45)) # absolute value
+print(float(-45)) # works with numbers and strings '-45'
+print(round(45.5899,1)) # (value,decimal place)
+print(type(.5))
+print(max(15,46,99))
+print(min(11,45,0))
+
+
+
+
+
+
+
+
